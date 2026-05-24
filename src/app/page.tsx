2d6d47794a3a667321a0c86bf33e1b3144de1e16
Varshitha596 <koilakondavarshapraveenkumar@gmail.com>
2026-05-24 22:41:53 +0530

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,76 @@
-import Link from "next/link"
-import Navbar from "./components/navbar"
-import Cart from "./components/card"
+//import Link from "next/link"
+//import Navbar from "./components/navbar"
+//import Cart from "./components/card"
+"use client"
+import { useState } from "react"
 export default function HOME(){
-const bloginfo=[
-    {
-        id:"1",
-        ti:"me",
-        di:"varsah"
-    },
-    {
-        id:"2",
-        ti:"me",
-        di:"varsah"
-    },
-    {
-        id:"3",
-        ti:"me",
-        di:"varsah"
-    }
-]
-    return ( 
-    <div>
-        <Navbar/>
-        <h1 >
-            hi.. varsha
-        </h1>
-        <Link href="/about">welcome to the about</Link><br/>
-        <Link href="/service">welcome to the service</Link><br/>
-        <Link href="/profile">welcome to the profile</Link><br/>
-        <Link href="/contact">welcome to the contact</Link>
+    const [count,funcount]=useState(0)
+    const [nam,setname]=useState('')
+    const [email,setemail]=useState('')
 
-    {
-bloginfo.map((i,id) => (
-<Cart
-key={id}
-titale={i.ti}
-des={i.di}
-id={i.id}
+    return (
+        <div>
+            <h1>{count}</h1>
+<button onClick={()=>funcount(count+1)}>in</button>
+<button onClick={()=>funcount(count-1)}>de</button>
+<input 
+type="text"
+placeholder="enter ur name"
+value={nam}
+onChange={(e) =>
+   setname( e.target.value)
+}
 />
-    ) ) }  
-    </div>
-)
+<br/>
+<input 
+type="password"
+placeholder="enter ur password"
+value={email}
+onChange={(e) =>
+   setemail( e.target.value)
+}
+/>
+<h1>{nam}</h1>
+  <h2>
+    {email}</h2> 
+    
+    
+    
+    <button type="submit"> Submit </button>
+
+         </div>
+
+    )
+// const bloginfo=[
+//     {
+//         id:"1",
+//         ti:"me",
+//         di:"varsah"
+//     },
+//     {
+//         id:"2",
+//         ti:"me",
+//         di:"varsah"
+//     },
+//     {
+//         id:"3",
+//         ti:"me",
+//         di:"varsah"
+//     }
+// ]
+//     return ( 
+//     <div>
+//         <Navbar/>
+//         <h1 >
+//             hi.. varsha
+//         </h1>
+//         <Link href="/about">welcome to the about</Link><br/>
+//         <Link href="/service">welcome to the service</Link><br/>
+//         <Link href="/profile">welcome to the profile</Link><br/>
+//         <Link href="/contact">welcome to the contact</Link>
+
+// { bloginfo.map((i,id) => (
+// <Cart key={id} titale={i.ti} des={i.di} id={i.id} />))}  
+//     </div>)
+
 }
